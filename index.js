af4c2195c3430ba0a9b1832229373bd68e66bb05
varunkumar1311 <113238019+varunkumar1311@users.noreply.github.com>
2022-10-06 13:24:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const http = require("http");
-const fs = require("fs");
+const hello = () => {
+  console.log("Hello Node.js!");
+};
 
-const server = http.createServer((req,res) => {
-    fs.readFile("sample.txt",(err,data) =>{
-        res.end(data);
-    })
-})
-server.listen(3000);   
+hello();
